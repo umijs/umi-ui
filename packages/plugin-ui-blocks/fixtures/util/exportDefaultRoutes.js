f3f7bb85baac2273557f60a31e9cc039d38c493d
ycjcl868 <45808948@qq.com>
2020-01-28 14:20:52 +0800

    fix: lint

--- packages/plugin-ui-blocks/fixtures/util/exportDefaultRoutes.js
+++ packages/plugin-ui-blocks/fixtures/util/exportDefaultRoutes.js
@@ -1,54 +1,54 @@
 export default {
   routes: [
     {
-      path: "/aa",
-      component: "aa",
+      path: '/aa',
+      component: 'aa',
       routes: [
         {
-          path: "/aa",
-          redirect: "/testredirect"
+          path: '/aa',
+          redirect: '/testredirect'
         },
         {
-          path: "/aa/vv",
-          name: "dd",
-          component: "adad" // 注释2
+          path: '/aa/vv',
+          name: 'dd',
+          component: 'adad' // 注释2
         },
         {
-          path: "/aa/xx",
-          name: "xx",
-          component: "xx",
+          path: '/aa/xx',
+          name: 'xx',
+          component: 'xx',
           routes: [
             {
-              path: "/aa/xx/sdad", //注释3
-              name: "aada",
-              component: "xxx",
+              path: '/aa/xx/sdad', //注释3
+              name: 'aada',
+              component: 'xxx',
               routes: [
                 {
-                  name: "hehe",
-                  path: "xxxcc"
+                  name: 'hehe',
+                  path: 'xxxcc'
                 }
               ]
             },
             {
-              path: "aa",
-              name: "aadsda",
-              component: "xxxc"
+              path: 'aa',
+              name: 'aadsda',
+              component: 'xxxc'
             }
           ]
         }
       ]
     },
     {
-      path: "/bb",
-      component: "bb"
+      path: '/bb',
+      component: 'bb'
     },
     {
-      path: "/",
-      component: "../MainLayout",
+      path: '/',
+      component: '../MainLayout',
       childRoutes: [
         {
-          path: "test1",
-          component: "./test1"
+          path: 'test1',
+          component: './test1'
         }
       ]
     }
