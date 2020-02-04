4ff6a0122cabc2f76d08f0fec030bd2f52529355
ycjcl868 <45808948@qq.com>
2020-02-04 12:30:34 +0800

    fix: test case

--- jest.config.js
+++ jest.config.js
@@ -7,14 +7,14 @@ module.exports = {
     process.env.E2E === 'none'
       ? [`${testMatchPrefix}/?*.(spec|test).(j|t)s?(x)`]
       : [`${testMatchPrefix}/?*.(spec|test|e2e).(j|t)s?(x)`],
-  testPathIgnorePatterns: ['/.git/', '/node_modules/', '/examples/', '/lib/'],
   moduleNameMapper: {
-    '^umi/_runtimePlugin$': require.resolve('umi/lib/runtimePlugin'),
-    '^@tmp/history': require.resolve('umi/lib/createHistory'),
+    '^@tmp/history': require.resolve('./history.js'),
   },
+  testPathIgnorePatterns: ['/.git/', '/node_modules/', '/examples/', '/lib/'],
   collectCoverageFrom: [
     `packages/${collectCoveragePrefix}/src/**/*.{js,jsx,ts,tsx}`,
     '!**/ui.umd.js',
+    '!packages/ui/client',
     '!packages/plugin-ui/src/bubble',
     '!**/fixtures/**',
     '!**/__snapshots__/**',
