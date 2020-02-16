0e39ce96dc08508f22326d718ba47bd3aaa496de
ycjcl868 <45808948@qq.com>
2020-02-17 00:25:31 +0800

    fix: script

--- scripts/uiPlugins.js
+++ scripts/uiPlugins.js
@@ -5,10 +5,10 @@ const uiPlugins = [
 
 const uiDist = [
   'packages/preset-ui/src/plugins/dashboard/dist/ui.umd.js',
-  'packages/preset-ui/src/plugins/configuration/dist/ui.umd.js',
+  // 'packages/preset-ui/src/plugins/configuration/dist/ui.umd.js',
   'packages/plugin-ui-tasks/dist/index.umd.js',
   'packages/plugin-ui-blocks/dist/index.umd.js',
-  'packages/ui/dist/index.umd.js',
+  // 'packages/ui/dist/index.umd.js',
   'packages/ui/client/dist/index.html',
 ];
 
