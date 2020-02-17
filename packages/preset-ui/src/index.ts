46d80d01d05cf79d043573f84d0850ff53fe0ccb
ycjcl868 <45808948@qq.com>
2020-02-17 11:25:33 +0800

    fix: umi ui env

--- packages/preset-ui/src/index.ts
+++ packages/preset-ui/src/index.ts
@@ -3,6 +3,11 @@ import { IApi } from 'umi';
 export default (api: IApi) => {
   // TODO: 区分生产和开发环境，生产环境引打包好的，或者通过异步远程加载也可以
   const injectBubble = process.env.NODE_ENV === 'development' && !api.userConfig.ssr;
+  if (process.env.UMI_UI === 'none') {
+    return {
+      plugins: [],
+    };
+  }
 
   return {
     plugins: [
