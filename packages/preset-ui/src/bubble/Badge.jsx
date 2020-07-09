33d71e3769a1a49b33183efcb6e6f462258f3b5e
乔磊 <qingquan.ql@antfin.com>
2020-07-09 16:29:50 +0800

    feat(ui):add bubble badge

---
+++ packages/preset-ui/src/bubble/Badge.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Badge = styled.sup`
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  box-shadow: 0 0 0 1px #fff;
+  width: 8px;
+  height: 8px;
+  background-color: #f00;
+  border-radius: 4px;
+`;
+
+export default Badge;
